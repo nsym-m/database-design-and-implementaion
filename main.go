fdaf4377eb4266f44c051997caa31759cc8131ae
nsym-m <mickey-ya@outlook.jp>
2026-03-20 16:09:40 +0900

    fix: mainではまだ何も実行しなくていいので中身を削除

--- main.go
+++ main.go
@@ -2,28 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/nsym-m/simpledb/internal/file"
 )
 
 func main() {
 	fmt.Println("hello world")
-	fm, err := file.NewFileManager("filetest", 400)
-	if err != nil {
-		panic(err)
-	}
-	block := file.NewBlockID("testfile", 2)
-	page1 := file.NewPage(fm.BlockSize())
-	pos1 := 88
-	text := "abcdefghijklm"
-	page1.SetString(pos1, text)
-	size := file.MaxLength(len(text))
-	pos2 := pos1 + size
-	page1.SetInt(pos2, 345)
-	fm.Write(*block, *page1)
-
-	page2 := file.NewPage(fm.BlockSize())
-	fm.Read(*block, *page2)
-	fmt.Printf("pos2 offset: %v, contains: %v\n", pos2, page2.GetInt(pos2))
-	fmt.Printf("pos1 offset: %v, contains: %v\n", pos1, page2.GetString(pos1))
 }
